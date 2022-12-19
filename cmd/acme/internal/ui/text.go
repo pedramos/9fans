pedramos/9fans

commit d4a8fea2804162da12f1fe8f09f1d3272d81a9ff

added patch #102

cmd/acme/internal/ui/text.go
@@ -539,13 +539,26 @@ func Textselect(t *wind.Text) {
 		}
 		adraw.Display.Flush()
 
-		for Mouse.Buttons == b {
-			*Mouse = <-Mousectl.C
-		}
+		// Mousectl.Read does both the Flush
+		// and the receive. We did the flush.
+		// Do just the receive, dropping biglock
+		// to let other goroutines proceed.
+		// Note that *Mouse is Mousectl.Mouse.
+		// We also need to release the window lock, or else other code
+		// will deadlock with us by acquiring the big lock and _then_ acquiring
+		// the window lock
+		o := t.W.Owner
+		wind.Winunlock(t.W)
+		BigUnlock()
+ 		for Mouse.Buttons == b {
+ 			*Mouse = <-Mousectl.C
+ 		}
 
-		clicktext = nil
-	}
-}
+		BigLock()
+		wind.Winlock(t.W, o)
+ 		clicktext = nil
+ 	}
+ }
 
 var BigLock = func() {}
 var BigUnlock = func() {}

cmd/acme/xfid.go
@@ -846,7 +846,13 @@ func xfideventwrite(x *Xfid, w *wind.Window) {
 			goto Rescue
 		}
 
-		wind.TheRow.Lk.Lock() // just like mousethread
+		o := w.Owner
+		wind.Winunlock(w)
+		bigUnlock()
+ 		wind.TheRow.Lk.Lock() // just like mousethread
+
+		bigLock()
+		wind.Winlock(w, o)
 
 		switch c {
 		case 'x',

commit 07b5f863b938fdc42cc46f72601c2723481f29a3

Fixed a on pasting to acme's header caused by d4a8fea

cmd/acme/internal/ui/text.go
@@ -547,15 +547,19 @@ func Textselect(t *wind.Text) {
 		// We also need to release the window lock, or else other code
 		// will deadlock with us by acquiring the big lock and _then_ acquiring
 		// the window lock
-		o := t.W.Owner
-		wind.Winunlock(t.W)
+		if t.W != nil {
+			wind.Winunlock(t.W)
+		}
+
 		BigUnlock()
  		for Mouse.Buttons == b {
  			*Mouse = <-Mousectl.C
  		}
 
 		BigLock()
-		wind.Winlock(t.W, o)
+		if t.W != nil {
+			wind.Winlock(t.W, t.W.Owner)
+		}
  		clicktext = nil
  	}
  }